ff1798654c1ffb043a531379ef5558ed1e5bb6b4
Jaabir Yusuf <jaabir-mahmoud@hotmail.com>
2022-02-22 11:18:26 +0000

    Second commit

--- tests/test_music_library.py
+++ tests/test_music_library.py
@@ -1,23 +1,27 @@
-from audioop import add
 import unittest
 
 from player.music_library import *
 
 
 class TestMusicLibrary(unittest.TestCase):
+    library = MusicLibrary()
+    track1 = Track("Title1", "Artist1", "Track1.mp4")
+    track2 = Track("Title2", "Artist2", "Track2.mp4")
+    
     def test_constructs(self):
         MusicLibrary()
 
-    def can_add_songs(self):
-        add("Song1")
-        add("Song2")
-        self.assertEqual(self.all ["Song1", "Song2"])
+    def test_can_add_songs(self):
+        self.library.add(self.track1)
+        self.library.add(self.track2)
+        self.assertEqual(self.library.all(), [self.track1, self.track2])
     
     def can_list_songs(self):
-        add("Song1")
-        self.assertEqual(self.all, ["Song1"])
+        self.library.add(self.track1)
+        self.assertEqual(self.library.all(), [self.track1])
 
     def can_remove_song(self):
-        add("Song1")
-        add("Song2")
-        self.assertEqual(self.remove(1), [])
+        self.library.add(self.track1)
+        self.library.add(self.track2)
+        self.assertEqual(self.library.remove(0), [])
+        self.assertEqual(self.library.all, [self.track2])
